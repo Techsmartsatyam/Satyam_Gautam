35c9880bd189105853e83986c1165104f7fe7792
Satyam Gautam <satyamsmartboy143@gmail.com>
2025-10-30 03:17:49 -0700

    Add files via upload

---
+++ port.js
@@ -0,0 +1,11 @@
+alert("Welcome To My Portfolio by ❤️💖💓💞")
+let card=document.querySelector(".card")
+let msg=document.querySelector("#massege")
+
+card.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const name=document.querySelector("#name")
+    const emali=document.querySelector("#email")
+message.textContent = "Thank you {name}! We received your email ({email})";
+ 
+});
